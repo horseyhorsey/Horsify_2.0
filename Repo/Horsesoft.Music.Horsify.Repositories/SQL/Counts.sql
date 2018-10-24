748cfd02a0825032e84e4fc8db2b98f81b6cd554
dave <dub.cyclone@gmail.com>
2018-10-24 23:56:54 +0100

    Add some sql scripts to save

--- Repo/Horsesoft.Music.Horsify.Repositories/SQL/Counts.sql
+++ Repo/Horsesoft.Music.Horsify.Repositories/SQL/Counts.sql
@@ -1,4 +1,17 @@
 ﻿
-SELECT COUNT(*) as Songs, COUNT(ArtistId) as Artists, COUNT(AlbumId) as Albums, COUNT(Country) as Countries, COUNT(DiscogId) as Discogs,
-COUNT(DISTINCT (ImageLocation == NULL)) as NoImages
-   FROM Song;
+
+-- Gets stats for database entries
+
+SELECT COUNT(F.Id) as FileCount, 
+       COUNT([Song].Id) as Songs, 
+       COUNT(DISTINCT ArtistId) as Artists, 
+       COUNT(DISTINCT AlbumId) as Albums, 
+       COUNT(DISTINCT Country) as Countries, 
+       COUNT(DISTINCT DiscogId) as Discogs,
+       COUNT(DISTINCT GenreId) as Genres,
+       COUNT(DISTINCT ImageLocation) as Images,
+       COUNT(DISTINCT LabelId) as Labels,
+       COUNT(DISTINCT Title) as Titles
+    From [File] as F  
+   Left Join [Song] On [Song].Id = F.Id;
+
---
+++ Repo/Horsesoft.Music.Horsify.Repositories/SQL/GetUntaggedFiles.sql
@@ -0,0 +1,6 @@
+﻿SELECT F.Id, S.FileId as SongFileId, F.FileName, F.Folder
+
+  FROM File as F
+  LEFT JOIN [Song] AS S ON F.Id = S.FileId
+  
+where S.FileId IS NULL;

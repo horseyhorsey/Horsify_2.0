e29fe1c62e31b9e5d5da54a486624b7af7d6fbe9
dave <dub.cyclone@gmail.com>
2018-10-29 16:25:33 +0000

    Refactor

--- UI/Modules/Horsesoft.Horsify.MediaPlayer/ViewModels/SongPlaying3dViewModel.cs
+++ UI/Modules/Horsesoft.Horsify.MediaPlayer/ViewModels/SongPlaying3dViewModel.cs
@@ -1,5 +1,4 @@
 ﻿using Horsesoft.Horsify.MediaPlayer.Model;
-using Horsesoft.Music.Horsify.Base;
 using Horsesoft.Music.Horsify.Base.Interface;
 using Prism.Events;
 using Prism.Logging;
@@ -11,33 +10,13 @@ namespace Horsesoft.Horsify.MediaPlayer.ViewModels
         private ISongPlayingInfo _songPlayingInfo;
 
         #region Constructors
-        public SongPlaying3dViewModel(ISongPlayingInfo songPlayingInfo, ILoggerFacade loggerFacade, IHorsifyMediaController horsifyMediaController, IEventAggregator eventAggregator, MediaControl mediaControl) : base(loggerFacade, horsifyMediaController, eventAggregator, mediaControl)
+        public SongPlaying3dViewModel(ILoggerFacade loggerFacade, IHorsifyMediaController horsifyMediaController, IEventAggregator eventAggregator, MediaControl mediaControl) : base(loggerFacade, horsifyMediaController, eventAggregator, mediaControl)
         {
-            _songPlayingInfo = songPlayingInfo;
-
-            //TODO Remove me
-            //_eventAggregator.GetEvent<OnMediaTimeChangedEvent<SongTime>>()
-            //.Subscribe(songTime =>
-            //{
-            //    if (!this._songPlayingInfo.IsSeeking)
-            //        CurrentSongTime = songTime.Duration.TotalSeconds;
-            //    CurrentSongPosition = songTime.CurrentSongTime.TotalSeconds;
-            //});
-
-            //Sets the IsSeeking variable
-            _eventAggregator
-                .GetEvent<OnMediaSeekEvent<bool>>().Subscribe(x =>
-                {
-                    _songPlayingInfo.IsSeeking = x;
-                });
-
-            //Fire when queue jobs complete
-            _eventAggregator.GetEvent<QueuedJobsCompletedEvent>().Subscribe(() =>
-            { ResetSelectedSong();}, ThreadOption.UIThread);
         } 
         #endregion
 
-        #region Song times
+        #region Properties
+
         private int _rating;
         public int Rating
         {
@@ -52,19 +31,5 @@ namespace Horsesoft.Horsify.MediaPlayer.ViewModels
 
         #endregion
 
-        #region Private Methods
-
-        /// <summary>
-        /// Resets the selected song by setting the song, positions and time to null
-        /// </summary>
-        private void ResetSelectedSong()
-        {
-            //TODO uncomment Song positions
-            MediaControlModel.SelectedSong = null;
-            //CurrentSongPosition = 0;
-            //CurrentSongTime = 0;
-            MediaControlModel.CurrentSongTimeString = null;
-        }
-        #endregion
     }
 }

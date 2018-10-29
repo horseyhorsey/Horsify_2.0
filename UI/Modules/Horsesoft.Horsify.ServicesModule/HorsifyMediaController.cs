928599c484eea44f1f53056115d50fbf14fdb1ec
dave <dub.cyclone@gmail.com>
2018-10-29 07:30:02 +0000

    Added a VLC implentation of the IHorsifyMediaController

---
+++ UI/Modules/Horsesoft.Horsify.ServicesModule/HorsifyMediaController.cs
@@ -0,0 +1,68 @@
+﻿using Horsesoft.Music.Horsify.Base;
+using Horsesoft.Music.Horsify.Base.Interface;
+using Horsesoft.Vlc;
+using System;
+
+namespace Horsesoft.Horsify.ServicesModule
+{
+    public class HorsifyVlcMediaController : IHorsifyMediaController
+    {
+        #region Fields
+        private static bool _isInitialized = false;
+        private IVlcPlayer _vlcPlayer;
+        #endregion
+
+        #region Events
+        public event OnMediaLoaded OnMediaLoaded;
+        public event OnTimeChanged OnTimeChanged;
+        #endregion
+
+        #region Constructors
+        public HorsifyVlcMediaController()
+        {
+            _vlcPlayer = new VlcPlayer();
+            _vlcPlayer.Init();
+            _isInitialized = true;
+
+            _vlcPlayer.MediaLoaded += (x) => OnMediaLoaded?.Invoke(x);
+            _vlcPlayer.TimeChanged += (x) => OnTimeChanged?.Invoke(x);
+        }
+        #endregion
+
+        #region Public Methods
+        public bool PlayPause(bool isPlaying)
+        {
+            if (!isPlaying)
+            {
+                _vlcPlayer.Play();
+                return true;
+            }
+            else
+            {
+                _vlcPlayer.Pause();
+                return false;
+            }
+        }
+
+        public void SetMedia(Uri file)
+        {
+            _vlcPlayer.SetMedia(file);
+        }
+
+        public void SetMediaPosition(double position)
+        {
+            _vlcPlayer.SetmediaPosition((float)position);
+        }
+
+        public void SetVolume(int currentVolume)
+        {
+            _vlcPlayer.SetVolume(currentVolume);
+        }
+
+        public void Stop()
+        {
+            _vlcPlayer.Stop();
+        }
+        #endregion
+    }
+}

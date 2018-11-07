2460e1f19b3817a1e9f6031079580b413b23d412
dave <dub.cyclone@gmail.com>
2018-11-07 21:16:46 +0000

    Should build service API and copy to correct build folder.

--- build.ps1
+++ build.ps1
@@ -1,11 +1,15 @@
 # BUILD X64 Jukebox
 msbuild "src/UI/Horsesoft.Music.Horsify.WPF.Shell/Horsesoft.Music.Horsify.WPF.Shell.csproj" /property:Configuration=Release /property:Platform=x64 /property:SolutionDir="../../../../../"
 
-# BUILD X86 Jukebox
+# # BUILD X86 Jukebox
 msbuild "src/UI/Horsesoft.Music.Horsify.WPF.Shell/Horsesoft.Music.Horsify.WPF.Shell.csproj" /property:Configuration=Release /property:Platform=x86 /property:SolutionDir="../../../../../"
 
-# BUILD X64 Song Importer
+# # BUILD X64 Song Importer
 msbuild "src/UI/Horsesoft.Music.Horsify.Importer.UI.WPF/Horsesoft.Music.Horsify.Importer.UI.WPF.csproj" /property:Configuration=Release /property:Platform=x64 /property:SolutionDir="../../../../../"
 
-# BUILD X86 Song Importer
+# # BUILD X86 Song Importer
 msbuild "src/UI/Horsesoft.Music.Horsify.Importer.UI.WPF/Horsesoft.Music.Horsify.Importer.UI.WPF.csproj" /property:Configuration=Release /property:Platform=x86 /property:SolutionDir="../../../../../"
+
+# Run the API build script (builds x86 and x64) (Copies to Build folder under Service)
+cd src/Services/Horsesoft.Music.Horsify.Api
+./build.ps1
--- src/Installer/Horsify2.Installer/ReadMe.md
+++ src/Installer/Horsify2.Installer/ReadMe.md
@@ -1,13 +1,15 @@
 ﻿# Daves Wix install notes
 
-### Run HEAT
+### Running HEAT
 
-Run Wix heat to generate components.
+Run Wix heat to generate components to be installed.
 
 Remove all "Directory="TARGETDIR" refs from generated file.
 
-Replace the Source attribute:  Source="SourceDir\e_sqlite3.dll"   to the build outputs
+Replace the Source attribute:  Source="SourceDir\e_sqlite3.dll" to the build outputs
 
 eg. Source="..\..\Build\$(var.Platform)\e_sqlite3.dll"
 
+
+Heat Command
 	heat dir ".\x64" -cg Binaries -srd -sreg -gg -out "JukeAndImporter.wxs"

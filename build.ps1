e19d3033e20084dce08d28841f7a0da24eb46d0a
dave <dub.cyclone@gmail.com>
2018-11-07 22:07:14 +0000

    Move building service till last. Should work......

--- build.ps1
+++ build.ps1
@@ -1,6 +1,3 @@
-# Run the API build script (builds x86 and x64) (Copies to Build folder under Service)
-cd src/Services/Horsesoft.Music.Horsify.Api
-./build.ps1
 
 # BUILD X64 Jukebox
 msbuild "src/UI/Horsesoft.Music.Horsify.WPF.Shell/Horsesoft.Music.Horsify.WPF.Shell.csproj" /property:Configuration=Release /property:Platform=x64 /property:SolutionDir="../../../../../"
@@ -14,3 +11,6 @@ msbuild "src/UI/Horsesoft.Music.Horsify.Importer.UI.WPF/Horsesoft.Music.Horsify.
 # # BUILD X86 Song Importer
 msbuild "src/UI/Horsesoft.Music.Horsify.Importer.UI.WPF/Horsesoft.Music.Horsify.Importer.UI.WPF.csproj" /property:Configuration=Release /property:Platform=x86 /property:SolutionDir="../../../../../"
 
+# Run the API build script (builds x86 and x64) (Copies to Build folder under Service)
+cd src/Services/Horsesoft.Music.Horsify.Api
+./build.ps1

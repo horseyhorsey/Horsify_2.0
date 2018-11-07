714884651645f2728442b8afaafb50a7f52cfbeb
dave <dub.cyclone@gmail.com>
2018-11-07 20:58:49 +0000

    add xcopy to services build script (x86 and x64). Try and run the services script after first build script is run

--- src/Services/Horsesoft.Music.Horsify.Api/build.ps1
+++ src/Services/Horsesoft.Music.Horsify.Api/build.ps1
@@ -1,3 +1,6 @@
 ﻿dotnet clean
 dotnet publish -c Release --self-contained --runtime win7-x86
+XCOPY /I /E /Y  ".\bin\Release\netcoreapp2.1\win7-x86\publish\*.*" "..\..\Build\x86\Service"
+
 dotnet publish -c Release --self-contained --runtime win7-x64
+XCOPY /I /E /Y  ".\bin\Release\netcoreapp2.1\win7-x64\publish\*.*" "..\..\Build\x64\Service"

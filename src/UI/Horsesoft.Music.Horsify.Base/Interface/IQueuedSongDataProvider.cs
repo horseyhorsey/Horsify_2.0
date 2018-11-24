adba6361b11a6eacddc966b84d77b88e81e7f7b6
dave <dub.cyclone@gmail.com>
2018-11-24 14:37:08 +0000

    QueueSongRange interface

--- src/UI/Horsesoft.Music.Horsify.Base/Interface/IQueuedSongDataProvider.cs
+++ src/UI/Horsesoft.Music.Horsify.Base/Interface/IQueuedSongDataProvider.cs
@@ -1,4 +1,6 @@
 ﻿using Horsesoft.Music.Data.Model;
+using Horsesoft.Music.Data.Model.Horsify;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 
 namespace Horsesoft.Music.Horsify.Base.Interface
@@ -6,11 +8,9 @@ namespace Horsesoft.Music.Horsify.Base.Interface
     public interface IQueuedSongDataProvider
     {
         bool Add(AllJoinedTable allJoinedTable);
-
         ObservableCollection<AllJoinedTable> QueueSongs { get; set; }
-
+        void QueueSongRange(IEnumerable<AllJoinedTable> songs, int amount);
         bool ShuffleEnabled { get; set; }
-
-        void Shuffle();
+        void Shuffle();        
     }
 }

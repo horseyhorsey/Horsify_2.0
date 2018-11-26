eae25d53338d97b9b8a5f225f98422b201d7d28b
dave <dub.cyclone@gmail.com>
2018-11-26 23:43:22 +0000

    Fix service not starting after running installer. Note: (dotnet) don't just comment out a nuget package in the csproj. Remove it correctly otherwise you end up with deps.json throwing error on publish. Created SharedAssemblyInfo to change file versions in one place.

---
+++ src/Shared/SharedAssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Markup;
+using System;
+
+[assembly: AssemblyVersion("2.0.0.100")]
+[assembly: AssemblyFileVersion("2.0.0.100")]
